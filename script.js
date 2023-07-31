9ad66b3bcc407c8773f26f958f5da70bb3649aa3
Josue Quinones <74983067+josue-quinones@users.noreply.github.com>
2023-07-31 12:30:00 -0400

    Added hovering effect to cell divs
    Also renamed grid div to cell

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const container = document.querySelector('div.container');
 const ORIGINAL_SIZE = 16
 
+function addHoverEffect(e) {
+    this.style.backgroundColor = "black";
+}
+
 function createGrid(size) {
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
-            const div = document.createElement('div');
-            div.classList.add("grid-square");
-            div.style.width = `calc(100% / ${size})`;
-            div.style.height = `calc(100% / ${size})`;
-            container.appendChild(div);
+            const cell = document.createElement('div');
+            cell.classList.add("grid-square");
+            cell.style.width = `calc(100% / ${size})`;
+            cell.style.height = `calc(100% / ${size})`;
+            container.appendChild(cell);
+
+            cell.addEventListener('mouseenter',addHoverEffect);
         }
     }
 }
