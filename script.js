d6524fb1a04b84d78b8bdb630be5b2f18bb58958
Josue Quinones <74983067+josue-quinones@users.noreply.github.com>
2023-07-31 12:49:56 -0400

    Add onclick functionality to button

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const container = document.querySelector('div.container');
-const ORIGINAL_SIZE = 16
+const button = document.querySelector('button');
+const MAX_SIZE = 100;
+const ORIGINAL_SIZE = 16;
 
 function addHoverEffect(e) {
     this.style.backgroundColor = "black";
 }
 
 function createGrid(size) {
+    if (size > 100) size = 100;
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             const cell = document.createElement('div');
@@ -17,6 +20,15 @@ function createGrid(size) {
             cell.addEventListener('mouseenter',addHoverEffect);
         }
     }
+}
+
+button.onclick = () => {
+    let newSize = parseInt(prompt("Change the grid size here: (MAX allowed is 100", 16));
+    let cells = [...container.childNodes];
+    for (let i = 0; i < cells.length; i++) {
+        container.removeChild(cells[i]);
+    }
+    createGrid(newSize);
 }
 
 createGrid(ORIGINAL_SIZE);
